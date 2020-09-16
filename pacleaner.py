cf195c5b30995233999e670cc55b2efb7df53fc9
lesebas <sebdeligny@gmail.com>
2020-09-16 19:26:20 +0200

    update to 0.2.2 to take in charge new package extension .zst

--- pacleaner.py
+++ pacleaner.py
@@ -40,7 +40,7 @@ else:
 
 NR_OF_PKG = int(config['DEFAULT']['Nb_Of_Pkg_Keep'])
 SECURE_DELETE = config.getboolean('DEFAULT' , 'Delete_Confirmation')
-EXTENSIONS = ["pkg.tar.xz", "pkg.tar.gzip"]
+EXTENSIONS = ["pkg.tar.xz", "pkg.tar.gzip" , "pkg.tar.zst"]
 ARCHES = ["any", "x86_64", "i686"]
 
 class Package(object):
